5745e3d9374f7a0d56a0ac3c5e40be425cfe8db0
Luis Rodriguez <luisalfonsozxca@gmail.com>
2026-05-26 00:07:22 -0500

    Se agrego el componente online

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,11 @@
 (function () {
     window.RankingFirebaseConfig = {
-        apiKey: "",
-        authDomain: "",
-        projectId: "",
-        storageBucket: "",
-        messagingSenderId: "",
-        appId: ""
+        apiKey:            "AIzaSyCF2sdmhbt-h1PspuWCZEHp2JQ-2A6Olrs",
+        authDomain:        "netcivility-c8683.firebaseapp.com",
+        databaseURL:       "https://netcivility-c8683-default-rtdb.firebaseio.com",
+        projectId:         "netcivility-c8683",
+        storageBucket:     "netcivility-c8683.firebasestorage.app",
+        messagingSenderId: "321390169763",
+        appId:             "1:321390169763:web:02b8fa680b079fb0617916"
     };
 })();
